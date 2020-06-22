4fd22d1ad71ab404909193098a17b802e9f8e246
rui tang <tassassin@sina.com>
2020-06-22 17:12:18 +0800

    clear

--- src/com/test/B.java
+++
@@ -1,8 +0,0 @@
-package com.test;
-
-/**
- * @author tr
- * @date 2020/6/22 17:05
- */
-public class B {
-}
--- src/com/test2/A.java
+++
@@ -1,8 +0,0 @@
-package com.test2;
-
-/**
- * @author tr
- * @date 2020/6/22 17:06
- */
-public class A {
-}
