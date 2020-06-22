421096d24d3a06fb3000a9702acfa78548e15abd
rui tang <tassassin@sina.com>
2020-06-22 17:20:42 +0800

    test change

---
+++ src/com/test/A.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * @author tr
+ * @date 2020/6/22 17:20
+ */
+public class A {
+    public static void main(String[] args) {
+        System.out.println("test change");
+    }
+}
